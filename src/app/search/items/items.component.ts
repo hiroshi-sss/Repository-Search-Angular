90c0573f3643ee075f31e26bf8f887947b872981
hiroshi <hiroshi.weblog@gmail.com>
2020-01-03 10:59:45 +0900

    ・select機能の追加

--- src/app/search/items/items.component.ts
+++ src/app/search/items/items.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/service/http.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-items',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private service: HttpService) {
+  }
+
+  private subscription: Subscription;
+  public item: any[];
+  items = [];
 
   ngOnInit() {
+    this.subscription = this.service.item$.subscribe(
+      data => this.item = data
+    )
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  deleteItem(len: number):void {
+    this.item.splice(len, 1);
+    this.service.itemData(this.item);
+    if ( this.item.length === 0 ) {
+      delete this.item;
+    }
+  }
+  
+  hoge() {
+    // console.log(this.item.length);
+  }
+
+  // selectItems(value: number) {
+  //   this.item.push(this.searches.items[value])
+  //   this.service.itemData(this.item);
+  // }
 
 }
