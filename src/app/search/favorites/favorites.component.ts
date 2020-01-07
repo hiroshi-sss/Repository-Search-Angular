1bf90d4d28551d8901f2612fa9cd44af91ebeebe
hiroshi <hiroshi.weblog@gmail.com>
2020-01-08 00:43:11 +0900

    ・お気に入り機能の追加

--- src/app/search/favorites/favorites.component.ts
+++ src/app/search/favorites/favorites.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesComponent implements OnInit {
 
+  public favorites: any;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentChecked(): void {
+    this.getFavorites();
+  }
+
+  getFavorites() {
+    this.favorites = JSON.parse(localStorage.getItem('favorites'))
+  }
+
+  deleteFavorites(len: number) {
+    this.favorites.splice(len, 1);
+    if (this.favorites.length === 0) {
+      localStorage.removeItem('favorites')
+      delete this.favorites;
+    } else {
+      localStorage.setItem('favorites', JSON.stringify(this.favorites));
+    }
   }
 
 }
