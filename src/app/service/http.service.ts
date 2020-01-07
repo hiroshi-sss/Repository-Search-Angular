95006e235cdcfce35008cec7bc06a4c8a18836e2
hiroshi <hiroshi.weblog@gmail.com>
2020-01-08 00:41:25 +0900

    ・不要コードの削除

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject, Subscription } from 'rxjs';
-import { ResultComponent } from '../search/result/result.component';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,17 +16,13 @@ export class HttpService {
   public item$ = this.items.asObservable();
   public search$ = this.searches.asObservable();
   public isLoading = new Subject<boolean>();
-  public isSelect = new Subject<boolean[]>();
-
 
   constructor(private http: HttpClient) { }
-
 
   onSearch(searchName: string): Observable<RepoItems[]> {
     const url = `${this.repositoriesUrl}${searchName}`;
     return this.http.get<RepoItems[]>(url);
   }
-
 
   public show(): void {
     this.isLoading.next(true);
@@ -35,16 +30,6 @@ export class HttpService {
 
   public hide(): void {
     this.isLoading.next(false);
-  }
-
-  public isSelectShow(i: number) {
-    console.log('TEST')
-    this.isSelect.next(false[i]);
-  }
-
-  public isSelectHide(i: number) {
-    console.log('TEST2')
-    this.isSelect.next(true[i]);
   }
 
   public searchData(searchName: RepoItems[]) {
