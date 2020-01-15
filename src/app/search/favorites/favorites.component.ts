9c07861ec14a61c695db0734578026b9ea0c805a
hiroshi <hiroshi.weblog@gmail.com>
2020-01-15 10:01:36 +0900

    リファクタリング

--- src/app/search/favorites/favorites.component.ts
+++ src/app/search/favorites/favorites.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RepoItems } from 'src/app/model/response-model';
 
 @Component({
   selector: 'app-favorites',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesComponent implements OnInit {
 
-  public favorites: any;
+  public favorites: RepoItems[];
   constructor() { }
 
   ngOnInit() {
